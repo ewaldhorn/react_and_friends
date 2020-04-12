77ab19fd5c8ac1bdf027ca5cf3af7f84f335cb26
Ewald Horn <ewaldhorn@gmail.com>
2020-04-12 11:47:49 +0200

    Created the basic stage and all the tetrominos.

--- hook_tetris/src/components/stage.js
+++ hook_tetris/src/components/stage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Cell from './cell';
 
-const Stage = ({stage}) => (
+const Stage = ({ stage }) => (
     <div>
-        <Cell/>
+        {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
     </div>
 )
 
