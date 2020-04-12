16f3a7504f9864d9bcbc8755fe32626145452489
Ewald Horn <ewaldhorn@gmail.com>
2020-04-12 14:17:30 +0200

    Basic styling and grid in place.

--- hook_tetris/src/components/cell.js
+++ hook_tetris/src/components/cell.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import {StyledCell} from './styles/styled_cell';
+import {TETROMINOS} from '../tetrominos';
 
 const Cell = ({type}) => (
-     <div>
-         cell
-     </div>
+     <StyledCell type={type} color={TETROMINOS[type].color}/>
 )
 
 export default Cell;
--- hook_tetris/src/components/tetris.js
+++ hook_tetris/src/components/tetris.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Stage from './stage';
 import Display from './display';
-import StartButton from './start_button'
-import {createStage} from '../game_helpers'
+import StartButton from './start_button';
+import { createStage } from '../game_helpers';
+import { StyledTetrisWrapper, StyledTetris} from './styles/styled_tetris';
 
 const Tetris = () => {
 
     return (
-        <div>
-            <Stage stage={createStage()}/>
-            <aside>
-                <div>
-                    <Display text='Score' />
-                    <Display text='Rows' />
-                    <Display text='Level' />
-                </div>
-                <StartButton />
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text='Score' />
+                        <Display text='Rows' />
+                        <Display text='Level' />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     )
 }
 
