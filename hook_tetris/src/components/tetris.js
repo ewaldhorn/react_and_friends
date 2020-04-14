4efdbc7d976aa0486bca470ba441b35deeaf9b8d
Ewald Horn <ewaldhorn@gmail.com>
2020-04-14 16:21:14 +0200

    Adding scores / scoring.

--- hook_tetris/src/components/tetris.js
+++ hook_tetris/src/components/tetris.js
@@ -7,13 +7,16 @@ import { StyledTetrisWrapper, StyledTetris } from './styles/styled_tetris';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 import { createStage, checkCollision } from '../game_helpers';
+import { useInterval } from '../hooks/useInterval';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-    const [stage, setStage] = useStage(player, resetPlayer);
+    const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+    const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
     const movePlayer = direction => {
         if (!checkCollision(player, stage, { x: direction, y: 0 })) {
@@ -23,11 +26,21 @@ const Tetris = () => {
 
     const startGame = () => {
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
         setGameOver(false);
+        setScore(0);
+        setRows(0);
+        setLevel(0);
     }
 
     const drop = () => {
+        // increase level and speed when player has cleared 10 rows
+        if (rows > (level + 1) * 10) {
+            setLevel(prev => prev + 1);
+            setDropTime(1000 / (level + 1) + 200);
+        }
+        // now check for collisions etc.
         if (!checkCollision(player, stage, { x: 0, y: 1 })) {
             updatePlayerPos({ x: 0, y: 1, collided: false });
         } else {
@@ -39,7 +52,17 @@ const Tetris = () => {
         }
     }
 
+    const keyUp = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 40) {
+                // down key released
+                setDropTime(1000 / (level + 1) + 200);
+            }
+        }
+    }
+
     const dropPlayer = () => {
+        setDropTime(null);
         drop();
     }
 
@@ -57,16 +80,21 @@ const Tetris = () => {
         }
     }
 
+    useInterval(() => {
+        drop();
+
+    }, dropTime)
+
     return (
-        <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
+        <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)} onKeyUp={keyUp}>
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
                     {gameOver ? <Display gameOver='{gameOver}' text='Game Over' /> :
                         (<div>
-                            <Display text='Score' />
-                            <Display text='Rows' />
-                            <Display text='Level' />
+                            <Display text={`Score: ${score}`} />
+                            <Display text={`Rows: ${rows}`} />
+                            <Display text={`Level: ${level}`} />
                         </div>)}
                     <StartButton callback={startGame} />
                     <ReactVersion />
--- hook_tetris/src/hooks/useStage.js
+++ hook_tetris/src/hooks/useStage.js
@@ -6,19 +6,34 @@ export const useStage = (player, resetPlayer) => {
     const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage =>
+            newStage.reduce((ack, row) => { 
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    // clear row because it is full
+                    setRowsCleared(prev => prev + 1);
+                    ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                return ack;
+            }, [])
+
         const updateStage = prevStage => {
             const newStage = prevStage.map(row => row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)));
             player.tetromino.forEach((row, y) => {
-                row.forEach((value, x) =>{
+                row.forEach((value, x) => {
                     if (value !== 0) {
-                        newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided? 'merged':'clear'}`]
+                        newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? 'merged' : 'clear'}`]
                     }
                 });
             });
 
             // check for collisions
-            if (player.collided){
+            if (player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
             }
 
             return newStage;
@@ -27,5 +42,5 @@ export const useStage = (player, resetPlayer) => {
         setStage(prev => updateStage(prev));
     }, [player, resetPlayer]);
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 }
